7bd352012037cd709d86789941fcd203b9d0a23f
Philipp Kalytta <git@kalytta.net>
2021-01-06 16:54:40 +0100

    * Optimization for rLUT() Function to not always traverse trough all if statements
    Signed-off-by: Philipp Kalytta <git@kalytta.net>

--- src/datasets/MidiParser.py
+++ src/datasets/MidiParser.py
@@ -5,6 +5,7 @@ import math
 from enum import IntEnum
 from time import perf_counter
 import concurrent.futures
+import itertools
 
 
 class Sound(IntEnum):
@@ -41,41 +42,7 @@ class MidiParser:
                 out += -1
         return out
 
-    def rLUT(key):
-        '''Turns an Integer between 0 and 87 into a String of the corresponding key name'''
-        octave = 0
-        offset = 0
-        if key == 0:
-            return 'A0'
-        if key == 1:
-            return 'A#0'
-        if key == 2:
-            return 'B0'
-        if key >= 3:
-            octave = 1
-            offset = 3
-        if key >= 15:
-            octave = 2
-            offset = 15
-        if key >= 27:
-            octave = 3
-            offset = 27
-        if key >= 39:
-            octave = 4
-            offset = 39
-        if key >= 51:
-            octave = 5
-            offset = 51
-        if key >= 63:
-            octave = 6
-            offset = 63
-        if key >= 75:
-            octave = 7
-            offset = 75
-        if key == 87:
-            octave = 8
-            offset = 87
-
+    def getKey(key : int, offset : int) -> str:
         key = {
             0: 'C',
             1: 'C#',
@@ -90,6 +57,47 @@ class MidiParser:
             10: 'A#',
             11: 'B'
         }.get(key-offset)
+        return key
+
+
+    def rLUT(key : int):
+        '''Turns an Integer between 0 and 87 into a String of the corresponding key name'''
+        octave = 0
+        offset = 0
+        if key == 0:
+            return 'A0'
+        elif key == 1:
+            return 'A#0'
+        elif key == 2:
+            return 'B0'
+        elif key in range(3,15) :
+            octave = 1
+            offset = 3
+        elif key in range(15,27):
+            octave = 2
+            offset = 15
+        elif key in range(27,39):
+            octave = 3
+            offset = 27
+        elif key in range (39,51):
+            octave = 4
+            offset = 39
+        elif key in range(51,63):
+            octave = 5
+            offset = 51
+        elif key in range(63, 75):
+            octave = 6
+            offset = 63
+        elif key in range(75,87):
+            octave = 7
+            offset = 75
+        elif key == 87:
+            octave = 8
+            offset = 87
+        else:
+            raise Exception()
+
+        key = MidiParser.getKey(key,offset)
 
         return key+str(octave)
 
@@ -119,6 +127,8 @@ class MidiParser:
         for i in range(0, self.length):
             now = midif.flat.getElementsByOffset(
                 i/4.0, i/4.0+0.25, includeEndBoundary=False, mustBeginInSpan=True, mustFinishInSpan=False)
+            if (i % 100 == 0):
+                print("Processing the {}th 16th note.".format(str(i)))
             for element in now.recurse():
 
                 if isinstance(element, tempo.MetronomeMark):  # BPM Mark
@@ -138,7 +148,7 @@ class MidiParser:
             self.arr[i][0] = currentBPM
         return self.arr
 
-    def createPart(arr : list, key : int) -> stream.Stream():
+    def createPart(arr: list, key: int) -> stream.Stream():
         """Creates a Part-Stream for the given key from the array
 
         Args:
@@ -169,25 +179,34 @@ class MidiParser:
                 keypart.insert(offset, theNote)
 
         print("Created Stream for Key: " + str(key))
-        return keypart
+        return keypart  # TODO: Must be pickleable, @see https://groups.google.com/g/music21list/c/VTy1cA2D1yw/m/wLyq7IKqBQAJ and https://github.com/cuthbertLab/music21/pull/304
 
-    def arrayToMidi(self, arr, filename):
+    def arrayToMidi(self, arr, filename: str):
         """Converst an array to a MIDI file concurrently for every possible of the 88 notes
 
         Args:
             arr (list): The array to convert to MIDI
             filename (str): The filename, where the MIDI file should be created
         """
-        theStream = stream.Score()
-        with concurrent.futures.ThreadPoolExecutor() as executor:
-            futures = [executor.submit(MidiParser.createPart,arr.copy(),key) for key in range(1,88+1)]
+        # Working with ThreadPoolExceutor, but much faster with ProcessPoolExecutor
+        # with concurrent.futures.ProcessPoolExecutor() as executor:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=32) as executor:
+            futures = [executor.submit(
+                MidiParser.createPart, *[arr, key]) for key in range(1, 88+1)]
 
-        keyparts = [future.result() for future in futures]
-        for keypart in keyparts:    
-            theStream.insert(0, keypart)
+            theStream = stream.Score()
 
-        # This function costs a lot of time
-        theStream.write('midi', fp=filename)
+            keyparts = [future.result() for future in futures]
+            for keypart in keyparts:
+                theStream.insert(0, keypart)
+                # TODO: Maybe create the part here, and only return a list of notes from the createPart function that is added to a part here
+                # vv This will work just fine, but theStream.write() further down won't work with ProcessPoolExecutor
+                #fn = filename.replace(".mid",str(keypart.id)+".mid")
+                # keypart.write("midi",fp=fn)
+
+            # This function costs a lot of time
+            theStream.show("text")
+            theStream.write('midi', fp=filename)
 
 
 def main():
@@ -214,6 +233,7 @@ def main():
     parser.arrayToMidi(song, "src/datasets/midi_parsed/" + file + "-new.mid")
     print("Array to Midi Time (ms): " + str(1000 * (perf_counter()-save)))
     print("Execution Time (ms): " + str(1000 * (perf_counter()-start)))
+
 
 if __name__ == "__main__":
     main()
